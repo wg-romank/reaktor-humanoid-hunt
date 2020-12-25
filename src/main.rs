1e8dab4c7622a38a62c8b8f419ec0fecd1456698
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2020-12-25 18:34:23 +0900

    A bit of style

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,22 @@ fn main() {
     let contents = std::fs::read_to_string("tatoo").expect("Failed to read input");
 
     for l in contents.lines() {
-        let bytes = l
+        let message = l
             .chars()
             .collect::<Vec<char>>()
             .iter()
-            .map(|c| if *c == '0' { 0 } else { 1 })
+            .map(|c| if *c == '0' { 0 } else { 1 }) // todo: any way to make it easier?
             .collect::<Vec<u8>>()
             .chunks(8)
-            .map(|c| c.iter().fold(0 as u8, |acc, n| {(acc << 1) ^ *n}))
+            .map(|c| c.iter().fold(0 as u8, |acc, n| {(acc << 1) ^ *n})) // big endian
             .collect::<Vec<u8>>();
 
-        let mut cursor: u8;
         let mut idx: u8 = 0;
+        let mut cursor: u8;
 
         loop {
-            cursor = bytes[idx as usize];
-            if cursor as usize <= bytes.len() {
+            cursor = message[idx as usize];
+            if (cursor as usize) < message.len() {
                 break
             }
 
@@ -28,8 +28,8 @@ fn main() {
         }
 
         loop {
-            if (cursor as usize) < bytes.len() {
-                cursor = bytes[cursor as usize];
+            if (cursor as usize) < message.len() {
+                cursor = message[cursor as usize];
             } else {
                 print!("{}", cursor as char);
                 break
